4431bdc951e21f478a97a92442ed8dd29eef29b9
chamiso <aschalew.chamiso@schrodinger.com>
2022-10-19 23:12:52 +0300

    BLDMGR-6439: Create script to check for required packages.
    Update based on comments from Felix and Nathan.

--- check_required_packages.sh
+++ check_required_packages.sh
@@ -1,67 +1,72 @@
-#!/bin/sh
+#!/bin/bash
 
-function identify_platform () {
-        platform=$(( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1)
-        echo "$platform"
+function identify_platform() {
+    echo "$(. /etc/os-release && echo "$ID")"
 }
 
-platform=$( identify_platform )
+platform=$(identify_platform)
+echo "platform is $platform"
+if [[ -z $platform ]]; then
+    echo "ERROR: This system couldn't be identified."
+    exit 1
+else
+    echo "INFO: This system was identified as \"$platform\"."
+fi
 
+function get_required_packages() {
+    platform=$1
 
-function get_required_packages () {
-    arg1=$1
+    case "${platform,,}" in
+    "centos" | "redhat" | "rocky")
+        required_packages=('fontconfig' 'libX11' 'libxkbcommon-x11' 'xcb-util-renderutil')
+        ;;
+    "ubuntu")
+        required_packages=('fontconfig')
+        ;;
+    *)
+        echo "WARNING: List of dependencies for distribition \"$platform\" is unknown."
+        return 1
+        ;;
+    esac
 
-    if [[ ${arg1,,} == *"centos"* ]];
-    then
-        required_packages=('fontconfig' 'libX11' 'libxkbcommon-x11' 'xcb-util-renderutil' 'test_package')
-    elif [[ ${arg1,,} == *"ubuntu"* ]];
-    elif [[ ${arg1,,} == *"redhat"* ]];
-    then
-        required_packages=('fontconfig', 'libX11', 'libxkbcommon-x11', 'xcb-util-renderutil')
-    elif [[ ${arg1,,} == *"rocky"* ]];
-    then
-        required_packages=('fontconfig', 'libX11', 'libxkbcommon-x11', 'xcb-util-renderutil'i)
-    fi
     echo "${required_packages[@]}"
 }
 
-required_packages=$( get_required_packages "$platform" )
+required_packages=$(get_required_packages "$platform")
 
+function check_if_package_installed() {
+    platform=$1
 
-function check_if_package_installed () {
-    arg1=$1
-    # Debian / Ubuntu
-    if [[ ${arg1,,} == *"debian"* || ${arg1,,}  == *"ubuntu"* ]];
-    then
+    case "$platform" in
+    "debian" | "ubuntu")
         cmd="dpkg -s"
-
-    # centos
-    elif [[ ${arg1,,} == *"centos"* ]];
-    then
+        ;;
+    "centos")
         cmd="yum list"
-
-    # redhat / Fedora / Suse
-    elif [[ ${arg1,,}  == *"redhat"* || ${arg1,,} == *"suse"* || ${arg1,,} == *"fedora"* ]];
-    then
+        ;;
+    "redhat" | "suse" | "fedora")
         cmd="rpm -qa | grep"
-    fi
+        ;;
+    *)
+        echo "WARNING: package check command for distribition \"$platform\" is unknown."
+        return 1
+        ;;
+    esac
+
     echo $cmd
 }
 
-package_check_cmd=$( check_if_package_installed "$platform" )
+package_check_cmd=$(check_if_package_installed "$platform")
 
-
-function get_missing_package () {
-    required_packages="$@"
+function get_missing_package() {
+    required_packages="$1"
     cmd="$2"
 
-    for package in ${required_packages};
-    do
+    for package in ${required_packages}; do
         echo "package: $package, cmd: $cmd"
         $cmd $package
-        if [[ $? != 0 ]];
-        then
-            echo "$package is a required package to use Schrödinger suite. 
+        if [[ $? != 0 ]]; then
+            echo "$package is a required package to use Schrödinger suite.
             Please install it using: sudo <package_manager place holder> $package."
         fi
     done
@@ -70,7 +75,6 @@ function get_missing_package () {
 echo "required_packages for your platform are: ${required_packages}"
 get_missing_package "${required_packages[@]}" "$package_check_cmd"
 
-
-function package_install_cmd () { 
-     # Do we want to suggest specific package manager per distro?
+function package_install_cmd() {
+    echo "Do we want to suggest specific package manager per distro?"
 }
