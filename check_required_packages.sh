5e970ec72116973db1db46f3b4a4b7f7b2bc9860
chamiso <aschalew.chamiso@schrodinger.com>
2022-10-21 12:31:52 +0300

    BLDMGR-6439: Suggest package install command.
    Suggest package install command if required packages are missing.

--- check_required_packages.sh
+++ check_required_packages.sh
@@ -38,13 +38,13 @@ function check_if_package_installed() {
     platform=$1
 
     case "$platform" in
-    "debian" | "ubuntu")
+    "ubuntu")
         cmd="dpkg -s"
         ;;
     "centos")
         cmd="yum list"
         ;;
-    "redhat" | "suse" | "fedora")
+    "redhat" | "suse")
         cmd="rpm -qa | grep"
         ;;
     *)
@@ -58,23 +58,42 @@ function check_if_package_installed() {
 
 package_check_cmd=$(check_if_package_installed "$platform")
 
+function get_package_manager() {
+    platform="$1"
+
+    case "$platform" in
+    "centos" | "redhat" | "rocky")
+        package_manager="yum"
+        ;;
+    "ubuntu")
+        package_manager="apt-get"
+        ;;
+    "suse")
+        package_manager="zypper"
+    *)
+        echo "WARNING: package manager for distribition \"$platform\" is unknown."
+        ;;
+    esac
+
+    echo "$package_manager"
+}
+
+package_manager=$(get_package_manager "$platform")
+
 function get_missing_package() {
     required_packages="$1"
     cmd="$2"
+    package_manager="$3"
 
     for package in ${required_packages}; do
         echo "package: $package, cmd: $cmd"
         $cmd $package
         if [[ $? != 0 ]]; then
             echo "$package is a required package to use Schrödinger suite.
-            Please install it using: sudo <package_manager place holder> $package."
+            Please install it using: sudo $package_manager install $package."
         fi
     done
 }
 
 echo "required_packages for your platform are: ${required_packages}"
-get_missing_package "${required_packages[@]}" "$package_check_cmd"
-
-function package_install_cmd() {
-    echo "Do we want to suggest specific package manager per distro?"
-}
+get_missing_package "${required_packages[@]}" "$package_check_cmd" "$package_manager"
