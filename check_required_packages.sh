b152a4c5cc7647f575a76fc1907e604ab4656ca8
Che <15792093+che-belew@users.noreply.github.com>
2022-11-14 22:50:01 +0300

    BLDMGR-6439: Create script to check for required packages. (#1)
    * BLDMGR-6439: Create script to check for required packages.
    
    Create script to check for required packages for Schrodinger suite on Linux distros.

---
+++ check_required_packages.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+function identify_platform() {
+    echo "$(. /etc/os-release && echo "$ID")"
+}
+
+platform=$(identify_platform)
+echo "platform is $platform"
+if [[ -z $platform ]]; then
+    echo "ERROR: This system couldn't be identified."
+    exit 1
+else
+    echo "INFO: This system was identified as \"$platform\"."
+fi
+
+function get_required_packages() {
+    platform=$1
+
+    case "${platform,,}" in
+    "centos" | "redhat" | "rocky")
+        required_packages=('fontconfig' 'libX11' 'libxkbcommon-x11' 'xcb-util-renderutil' 'libglvnd-egl' 'libglvnd-opengl' 'libxkbcommon')
+        ;;
+    "ubuntu")
+        required_packages=('fontconfig' 'libegl1' 'libopengl0' 'libxkbcommon0' 'libxcb-xinput0')
+        ;;
+    *)
+        echo "WARNING: List of dependencies for distribition \"$platform\" is unknown."
+        echo "Please install libX11 and/or libfontconfig packages."
+        return 1
+        ;;
+    esac
+
+    echo "${required_packages[@]}"
+}
+
+required_packages=$(get_required_packages "$platform")
+
+function check_if_package_installed() {
+    platform=$1
+
+    case "$platform" in
+    "ubuntu")
+        cmd="dpkg -s"
+        ;;
+    "centos")
+        cmd="yum list"
+        ;;
+    "redhat" | "suse")
+        cmd="rpm -qa | grep"
+        ;;
+    *)
+        echo "WARNING: package check command for distribition \"$platform\" is unknown."
+        return 1
+        ;;
+    esac
+
+    echo $cmd
+}
+
+package_check_cmd=$(check_if_package_installed "$platform")
+
+function get_package_manager() {
+    platform="$1"
+
+    case "$platform" in
+    "centos" | "redhat" | "rocky")
+        package_manager="yum"
+        ;;
+    "ubuntu")
+        package_manager="apt-get"
+        ;;
+    "suse")
+        package_manager="zypper"
+        ;;
+    *)
+        echo "WARNING: package manager for distribition \"$platform\" is unknown."
+        ;;
+    esac
+
+    echo "$package_manager"
+}
+
+package_manager=$(get_package_manager "$platform")
+
+function get_missing_package() {
+    required_packages="$1"
+    cmd="$2"
+    package_manager="$3"
+    missing_packages=()
+
+    for package in ${required_packages}; do
+        $cmd $package
+        if [[ $? != 0 ]]; then
+            missing_packages+=($package)
+        fi
+    done
+
+    if [ ${#missing_packages[@]} -eq 0 ]; then
+        echo "Your machine has the required packages."
+    else
+        echo "(${missing_packages[@]}) is/are required package/s to use Schrödinger suite. Please install them using:
+        sudo $package_manager install ${missing_packages[@]}"
+        exit 1
+    fi
+}
+
+echo "required_packages for your platform are: ${required_packages}"
+get_missing_package "${required_packages[@]}" "$package_check_cmd" "$package_manager"
