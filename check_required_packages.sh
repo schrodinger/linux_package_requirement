35a207110d554883754b81edac136a319fad8c4b
Karthik Rajagopalan <karthik.rajagopalan@schrodinger.com>
2026-01-07 15:29:30 -0500

    Support Ubuntu 24.04 and t64 package transitions INST-2211 (#46)
    * Separate ubuntu24 canonical requirements to distinguish t64 package suffix from older INST-2211
    
    - Starting ubuntu24 some packages were named with t64 suffix and expect them to be used
    instead of non-t64 suffix. These are not queryable by dpkg otherwise.
    
    * Organized code to move the functions and caller into separate section; And improved docs for readbility.

--- check_required_packages.sh
+++ check_required_packages.sh
@@ -2,21 +2,18 @@
 
 set -euo pipefail
 
+# Detects the OS distribution and version.
 function identify_platform() {
     platform="$(. /etc/os-release && echo "$ID")"
     version_id="$(. /etc/os-release && echo "$VERSION_ID")"
     major_version="${version_id%%.*}"
 }
 
-identify_platform
-
-if [[ -z "$platform" ]]; then
-    echo "ERROR: This system couldn't be identified."
-    exit 1
-else
-    echo "INFO: This system was identified as \"$platform\"."
-fi
-
+# Determines which packages are required based on the distribution or the external requirements file fed as input.
+#
+# Basic: Requirements needed for the distribution is checked if no input file is provided.
+# Extensive: For extensive check for the release, the package requirements of the release corresponding to the linux distribution
+# is fed as an argument to the script.
 function get_required_packages() {
     if [[ $# -gt 0 ]]; then
         package_file=$1
@@ -44,8 +41,7 @@ function get_required_packages() {
     esac
 }
 
-get_required_packages "$@"
-
+# Selects the command according to the OS to check if a package is installed.
 function check_if_package_installed() {
     case "$platform" in
     "ubuntu")
@@ -60,8 +56,7 @@ function check_if_package_installed() {
     esac
 }
 
-check_if_package_installed
-
+# Determines the appropriate package manager according to the OS.
 function get_package_manager() {
 
     case "$platform" in
@@ -83,8 +78,7 @@ function get_package_manager() {
     esac
 }
 
-get_package_manager
-
+# Identifies if additional repositories (like EPEL) are needed
 function get_extra_repos() {
   extra_repos=()
   case "$platform" in
@@ -96,8 +90,7 @@ function get_extra_repos() {
     esac
 }
 
-get_extra_repos
-
+# Iterate through required packages and suggests how to install if missing.
 function get_missing_package() {
     missing_packages=()
     for package in ${required_packages[@]}; do
@@ -118,6 +111,22 @@ function get_missing_package() {
     fi
 }
 
-echo -e "required_packages for your platform are: \n${required_packages[@]}"
+## Main execution
 
+# Identify the platform and version
+identify_platform
+if [[ -z "$platform" ]]; then
+    echo "ERROR: This system couldn't be identified."
+    exit 1
+else
+    echo "INFO: This system was identified as \"$platform\"."
+fi
+
+# Get the required packages for the platform and check if they are installed
+
+get_required_packages "$@"
+check_if_package_installed
+get_package_manager
+get_extra_repos
+echo -e "required_packages for your platform are: \n${required_packages[@]}"
 get_missing_package
